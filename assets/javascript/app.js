e8446f5779834c5ad80007aefe0975d85744b63c
Joseph Salutric <salutric8155@yahoo.com>
2017-06-06 15:42:44 -0400

    files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,7 +19,7 @@
   var dateTime = date+' '+time;
     console.log(time);
     $("#time").html("Current time: " + time);*/
-    function updateClock() {
+function updateClock() {
     var now = new Date(), // current date
         months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
         time = now.getHours() + ':' + now.getMinutes() + ":" + now.getSeconds();
@@ -39,25 +39,25 @@ $("#submit").on("click", function() {
       // Don't refresh the page!
       event.preventDefault();
 
-var trainName = $("#train-name").val().trim();
-var destination = $("#destination").val().trim();
-var firstTrain = $("#first-train").val().trim();
-var frequency = $("#frequency").val().trim();
+      var trainName = $("#train-name").val().trim();
+      var destination = $("#destination").val().trim();
+      var firstTrain = $("#first-train").val().trim();
+      var frequency = $("#frequency").val().trim();
 
-database.ref().push({
-  trainName: trainName,
-  destination: destination,
-  firstTrain: firstTrain,
-  frequency: frequency
-})
+      database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+      })
 
 
-/*$("#addedTrain").html(trainName + ":" + " " + destination + " " + "at" + " " + firstTrain + " " + frequency);*/
-$("form")[0].reset();
+      /*$("#addedTrain").html(trainName + ":" + " " + destination + " " + "at" + " " + firstTrain + " " + frequency);*/
+      $("form")[0].reset();
 });
 
 
-database.ref().on("value", function(snapshot) {
+database.ref().on("child_added", function(snapshot) {
 
       // Log everything that's coming out of snapshot
       console.log(snapshot.val());
@@ -66,7 +66,7 @@ database.ref().on("value", function(snapshot) {
       console.log(snapshot.val().firstTrain);
       console.log(snapshot.val().frequency);
 
-      // // Change the HTML to reflect
+      // Change the HTML to reflect
       // $("#name-display").html(snapshot.val().name);
       // $("#email-display").html(snapshot.val().email);
       // $("#age-display").html(snapshot.val().age);
